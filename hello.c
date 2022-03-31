be6a3ea3b863ec730e44066953a62a1fc464fd25
Andrew950623 <11011015@student.kjsh.ntpc.edu.tw>
2022-03-31 10:18:31 +0000

    update

--- hello.c
+++ hello.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
-int main(void){
-    printf("hello world \n");
+using namespace std;
+
+int main()
+{
+    int a,i;
+
+
+    printf(" please unput a number \n");
+    scanf("%d" ,&a);
+    if( a > 9 ){
+        printf(" a > 9 , hello \n");
+    }else{
+    printf(" hello world \n");
+    }
+    return 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,7 @@
 //  This is CodingCoffee for C/C++ simple hello world gitpod example
 //  more courses : www.codingcoffee.org
+
+
+git add . 
+git commit -m "update"
+git push
